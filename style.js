b381c30fb5e9c1533431509c6f6055953d28be21
sabina <prajasabina022@gmail.com>
2025-05-06 07:49:53 +0545

    js added

--- style.js
+++ style.js
@@ -4,3 +4,15 @@ const navLinks = document.getElementById('navLinks');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+const text = "Hi, I'm Mr.s";
+const typedText = document.getElementById('typed-text');
+let index = 0;
+
+function type(){
+  if(index < text.length){
+    typedText.textContent += text.charAt(index);
+    index++;
+    setTimeout(type,150);
+  }
+}
+type();
