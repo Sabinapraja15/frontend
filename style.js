ac08c80722cef2756933f0ba907bd2411a520bca
sabina <prajasabina022@gmail.com>
2025-05-08 07:46:40 +0545

    js modify

--- style.js
+++ style.js
@@ -4,7 +4,7 @@ const navLinks = document.getElementById('navLinks');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
-const text = "Hi, I'm Mr.s";
+const text = "Hi, I'm Ms.Sabina";
 const typedText = document.getElementById('typed-text');
 let index = 0;
 
@@ -16,3 +16,35 @@ function type(){
   }
 }
 type();
+
+//projects
+
+const projects = [
+  {
+    image: "rabbit.jpg",
+    title: "Animated Navbar",
+    description: "A simple animated navbar using HTML, CSS, and JavaScript.",
+  },
+  {
+    image: "rabbit.jpg",
+    title:"HTML CSS",
+    description: "A simple HTML CSS project using HTML, CSS, and JavaScript.",
+ },
+ {
+  image: "rabbit.jpg",
+  title: "Landing Pages",
+  description: "A simple landing page using HTML, CSS, and JavaScript.",
+ },
+];
+const container = document.getElementById("project-container");
+
+projects.forEach(project=>{
+  const card = document.createElement("div");
+  card.className = "project-card";
+  card.innerHTML= `
+  <img src="${project.image}" alt="${project.title}"/>
+  <h4>${project.title}</h4>
+  <p>${project.description}</p>
+    `;
+    container.appendChild(card);
+});
